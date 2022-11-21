eeea552e261005c487ef46c47c05af62d23d9c2d
Luillin Escobar <cataplum.17@hotmail.com>
2022-11-21 18:42:41 -0500

    Navbar footer and clean

--- src/app/modules/news/news.module.ts
+++ src/app/modules/news/news.module.ts
@@ -3,23 +3,19 @@ import { CommonModule } from '@angular/common';
 
 import { NewsRoutingModule } from './news-routing.module';
 import { PostListComponent } from './components/post-list/post-list.component';
-import { NormalPostComponent } from './components/normal-post/normal-post.component';
 import { NewsLayoutComponent } from './components/news-layout/news-layout.component';
 import {CoreModule} from "../core/core.module";
 import { ShowModalComponent } from './components/show-modal/show-modal.component';
 import { PostFormComponent } from './components/post-form/post-form.component';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import { ImagesFormComponent } from './components/images-form/images-form.component';
 
 
 @NgModule({
   declarations: [
     PostListComponent,
-    NormalPostComponent,
     NewsLayoutComponent,
     ShowModalComponent,
     PostFormComponent,
-    ImagesFormComponent,
   ],
   imports: [
     CommonModule,
